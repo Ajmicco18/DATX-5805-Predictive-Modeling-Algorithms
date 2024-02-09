13f75e647d9840ebe3594fe325e503796395fd0c
Anthony Micco <anthonymicco2003@gmail.com>
2024-02-09 12:52:14 -0500

    Started Assignment 3

--- Assignment 3/Assignment 3.R
+++ Assignment 3/Assignment 3.R
@@ -1,3 +1,28 @@
 library("readxl")
 
 mydata <- read_excel("Assignment3_HeartDisease.xlsx")
+
+set.seed(1)
+train.size = floor(0.7*nrow(mydata))
+ind = sample(1:nrow(mydata),train.size, replace=F)
+
+train = mydata[ind,]
+test = mydata[-ind,]
+
+logit.m = glm(HeartDisease ~ Age + Gender + ChestPainType + RestingBP +FastingBS, 
+              data=train,family="binomial")
+summary(logit.m)
+
+new_data 
+      
+predicted_prob = predict(logit.m, newdata = new_data, type = "response")
+predicted_prob
+
+pred_prob.test = predict(logit.m,newdata=test,type="response")
+predicted.test =rep(0,nrow(test))
+predicted.test[pred_prob.test > 0.5]=1
+
+table(test$HeartDisease, predicted.test)
+
+errorRate.test = mean(test$HeartDisease != predicted.test)
+errorRate.test
