fdf52f82accbf13bd3a004a616199e11e796e4fe
Anthony Micco <anthonymicco2003@gmail.com>
2024-03-20 12:26:48 -0400

    started Assignment 5

---
+++ Assignment 5.R
@@ -0,0 +1,33 @@
+data("College", package="ISLR2")
+
+library("leaps")
+
+fit.bestsub = regsubsets(Apps ~., data=College)
+summary(fit.bestsub)
+
+n.var = 12
+fit.bestsub = regsubsets(Apps ~.,data=College,nvmax=n.var)
+summary(fit.bestsub)
+
+#Looking for smallest cp, bic, aic for best fit
+#Looking for biggest adjusted r squared for best
+bestsubSummary = summary(fit.bestsub)
+
+#bic best fit
+bestsubSummary$bic
+plot(bestsubSummary$bic,xlab="Number of variables",
+     ylab="BIC", type="l")
+which.min(bestsubSummary$bic)
+points(10,bestsubSummary$bic[10],col="red",cex=1,pch=19)
+coef(fit.bestsub, 10)
+
+#adjusted r squared best fit
+bestsubSummary$adjr2
+plot(bestsubSummary$adjr2, xlab="Number of variables",
+     ylab="Adjusted R Squared", type="l")
+which.max(bestsubSummary$adjr2)
+points(12, bestsubSummary$adjr2[12], col="red", cex=1,pch=19)
+coef(fit.bestsub, 12)
+
+#forward stepwise selection
+
