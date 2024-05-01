795f3d03f3291cd3fd2640739fcbec219b085ba1
Anthony Micco <anthonymicco2003@gmail.com>
2024-05-01 12:59:26 -0400

    fitted logistic regression model

--- term-project/term-project.R
+++ term-project/term-project.R
@@ -7,3 +7,13 @@ set.seed(1)
 ind = sample(1:n, size = smp_size) 
 train.NBA = nba[ind, ]  # training data 
 test.NBA = nba[-ind, ]  # test data
+
+#fitting logistic model
+library("nnet")
+logit.nba <- multinom(all_nba ~ g + gs + mp_per_game + fg_percent+
+                       x3p_percent + x2p_percent + ft_percent +
+                        trb_per_game + ast_per_game + stl_per_game +
+                        blk_per_game + tov_per_game + pts_per_game,
+                      data = train.NBA)
+
+summary(logit.nba)
