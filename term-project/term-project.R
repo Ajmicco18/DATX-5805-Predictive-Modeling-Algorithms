fa0325adfc0e7db0bc0c4be4a2f84d25b7ce6eff
Anthony Micco <anthonymicco2003@gmail.com>
2024-05-02 16:12:55 -0400

    made slight changes to regression function

--- term-project/term-project.R
+++ term-project/term-project.R
@@ -1,17 +1,30 @@
 nba <- read.csv("nba_data.csv")
 
-#Creating test and train data sets
+#Further cleaning the data
 n=nrow(nba) 
-smp_size = ceiling(0.5 * n)  
-set.seed(1) 
-ind = sample(1:n, size = smp_size) 
-train.NBA = nba[ind, ]  # training data 
-test.NBA = nba[-ind, ]  # test data
+nba$all_nba[nba$all_nba == "False"] <- 0
+nba$all_nba[nba$all_nba == "True"] <- 1
+str(nba)
+nba$all_nba  <- as.numeric(nba$all_nba)
+str(nba)
 
-cleanTrain.NBA = na.omit(train.NBA)
-cleanTest.NBA = na.omit(test.NBA)
+#Creating test and train data sets
+set.seed(1)
+train.size = ceiling(0.5 * n)
+ind = sample(1:nrow(nba),train.size, replace=T)
+train = nba[ind,]  # training data 
+test = nba[-ind,] # test data
 
-#fitting logistic model
-library("nnet")
-logit.nba <- multinom(all_nba ~ .,data = train.NBA)
-summary(logit.nba)
+#Fitting the multiple logistic regression model
+logit.NBA = glm(all_nba ~.,data=train,family="binomial")
+summary(logit.NBA)
+
+#creating confusion matrix to evaluate
+pred_NBA.test = predict(logit.NBA,newdata=test, type="response")
+predicted.test = rep(0,nrow(test))
+predicted.test[pred_NBA.test > 0.5]=1
+
+table(test$all_nba, predicted.test)
+
+errorRate.test = mean(test$all_nba != predicted.test)
+errorRate.test
